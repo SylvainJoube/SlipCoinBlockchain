a86e30e7b0ead5ac73de01eabbaab49b668c22cb
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-01-14 23:44:15 +0100

    dernier commit avant la nuit

--- src/slip/blockchain/pos/SCCoinWallet.java
+++ src/slip/blockchain/pos/SCCoinWallet.java
@@ -1,6 +1,8 @@
 package slip.blockchain.pos;
 
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import slip.security.common.RSA;
 import slip.security.common.RSAKey;
@@ -9,6 +11,8 @@ public class SCCoinWallet {
 	
 	private String privateKeyAsString; // secrète, est la clef du coffre de l'utilisateur
 	private String publicKeyAsString;
+	private PrivateKey privateKey;
+	private PublicKey publicKey;
 	
 	public static SCCoinWallet createNewWallet()  {
 		KeyPair keys = null;
@@ -26,6 +30,8 @@ public class SCCoinWallet {
 	public SCCoinWallet(String arg_privateKey, String arg_publicKey) {
 		privateKeyAsString = arg_privateKey;
 		publicKeyAsString = arg_publicKey;
+		privateKey = RSAKey.loadPrivateKey(privateKeyAsString);
+		publicKey = RSAKey.loadPublicKey(publicKeyAsString);
 		
 	}
 	
--- src/slip/blockchain/pos/SCNode.java
+++ src/slip/blockchain/pos/SCNode.java
@@ -240,7 +240,6 @@ public class SCNode {
 	}
 	
 	
-	public 
 	
 	
 	
